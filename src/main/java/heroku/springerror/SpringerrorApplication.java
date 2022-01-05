65edff1dbcb889e3a43c296b18b9cbc54cfe5f66
Brian Leung <hyleung111one@yahoo.com>
2022-01-05 15:07:53 +0800

    main

---
+++ src/main/java/heroku/springerror/SpringerrorApplication.java
@@ -0,0 +1,13 @@
+package heroku.springerror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringerrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringerrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heroku/springerror/SpringerrorApplicationTests.java
@@ -0,0 +1,13 @@
+package heroku.springerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringerrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
